65d106e07b57df8040c2255ad0115758c8c3e542
ShiroNoTori <accelerator1569@gmail.com>
2019-07-23 15:20:40 +0300

    new branch improved

--- src/model/RomanNumber.java
+++ src/model/RomanNumber.java
@@ -25,24 +25,16 @@ public class RomanNumber implements AbstractNumber {
 
     @Override
     public String getString() {
-        return String.valueOf(new char[getInt()]).replace('\0', 'I')
-                .replace("IIIII", "V")
-                .replace("IIII", "IV")
-                .replace("VV", "X")
-                .replace("VIV", "IX")
-                .replace("XXXXX", "L")
-                .replace("XXXX", "XL")
-                .replace("LL", "C")
-                .replace("LXL", "XC")
-                .replace("CCCCC", "D")
-                .replace("CCCC", "CD")
-                .replace("DD", "M")
-                .replace("DCD", "CM");
+        return replace(String.valueOf(new char[getInt()]));
     }
 
     @Override
     public void setValue(int value) {
-        number = String.valueOf(new char[value]).replace('\0', 'I')
+        number = replace(String.valueOf(new char[value]));
+    }
+
+    private String replace(String str) {
+        return str.replace('\0', 'I')
                 .replace("IIIII", "V")
                 .replace("IIII", "IV")
                 .replace("VV", "X")
